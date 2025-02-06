33a19ad1119f9f3d97dfa537609a34a75baa35f3
xuhaoqing <xuhaoqing2013@hotmail.com>
2025-02-06 15:50:00 +0800

    add comments

--- inference.py
+++ inference.py
@@ -21,7 +21,7 @@ from model import create_model  # Ensure this is correctly imported from your mo
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 MODEL_TYPES = {
-    'total': ['adenoma', 'sessile serrated lesion', 'hyperplastic polyp'],
+    # 'total': ['adenoma', 'sessile serrated lesion', 'hyperplastic polyp'],
     'cloud': ['cloud', 'no_cloud'],
     'dark': ['dark', 'no_dark'],
     'dark_v': ['dark_v', 'no_dark_v'],
@@ -29,6 +29,71 @@ MODEL_TYPES = {
     'tubular': ['tubular', 'no_tubular'],
     'vague': ['vague', 'no_vague'],
     # 'spots': ['spots', 'no_spots'],
+}
+COMMENTS = {  # [type_name]: [neg_list, pos_list], both lists are [0-0.33, 0.33-0.66, 0.66-1]
+    'dark': [[
+        '病变颜色与背景粘膜差异不大，可能因为光线与拍摄角度难以分辨',
+        '病变颜色和背景粘膜区别很小，可以认为一样',
+        '病变颜色和背景粘膜一致，难以区分',
+    ], [
+        '病变颜色与背景粘膜差异不大，可能因为光线与拍摄角度难以分辨',
+        '病变颜色和背景粘膜存在差异，较背景粘膜更深',
+        '病变颜色明显比背景粘膜深',
+    ]],
+    'irregular': [[
+        '病变形状尚且规则，可能因拍摄角度存在误差',
+        '病变形状比较规则，对称',
+        '病变形状十分规则，对称',
+    ], [
+        '病变形状轻微不规则，可能因拍摄角度存在误差',
+        '病变形状不规则，可以看到分叶或是不同方向生长情况不一',
+        '病变形状明显不规则，表面分叶结构清晰可见',
+    ]],
+    'vague': [[
+        '尚能从背景粘膜中划分出病变区域',
+        '病变与背景粘膜间可以划分出分界线',
+        '病变与背景粘膜间可见明显、清晰的分界线',
+    ], [
+        '病变可能和背景粘膜融合，不易区分',
+        '很难从背景粘膜中精确划出病变区域，在某些角度，病变与背景粘膜的分界难以划分',
+        '病变与背景粘膜互相融合，完全无法指出分界线',
+    ]],
+    'dark_v': [[
+        '病变中可能没有明显的异常深色血管',
+        '在病变的大部分区域没有观察到异常的深色血管存在',
+        '病变中完全没有异常的深色血管',
+    ], [
+        '病变中可能存在深色血管',
+        '病变中可以看到围绕腺管的异常深色血管',
+        '病变中可以看到十分明显的异常深色血管',
+    ]],
+    'cloud': [[
+        '病变中可能没有云雾状表面结构，可能受光线与观察角度影响',
+        '在病变的大部分区域都没有观察到云雾状边缘',
+        '病变完全没有云雾状边缘',
+    ], [
+        '病变中可能存在云雾状表面结构，可能受光线与观察角度影响',
+        '在病变中可以看到云雾状表面结构',
+        '在病变中可以看到清晰、明显的云雾状表面结构',
+    ]],
+    'spots': [[
+        '病变中可能没有隐窝内黑点，可能受光线与观察角度影响',
+        '在病变的大部分区域都没有观察到隐窝内异常黑点',
+        '完全不可见隐窝内黑点',
+    ], [
+        '病变中可能存在隐窝内黑点，可能受光线与观察角度影响',
+        '在病变中可以看到隐窝内黑点',
+        '在病变中可以看到清晰、明显的隐窝内黑点',
+    ]],
+    'tubular': [[
+        '病变中可能没有管状/树枝状的异常腺管，可能受光线影响',
+        '在病变的大部分区域都没有观察到管状/树枝状异常腺管',
+        '病变中完全不存在管状/树枝状异常腺管',
+    ], [
+        '病变中可能存在管状/树枝状的异常腺管，可能受光线影响',
+        '病变中存在管状/树枝状异常血管，被深色血管包围',
+        '病变中可见明显管状/树枝状异常血管，被深色血管包围，清晰可见',
+    ]],
 }
 
 def inference_all(image_url):
@@ -148,19 +213,19 @@ def inference(image_url, model_type, image=None):
             
             if max_score < 1/3:
                 if pred_class == 'no_' + model_type:
-                    result['comment'] = '可能没有'
+                    result['comment'] = COMMENTS[model_type][0][0]
                 else:
-                    result['comment'] = '可能有'
+                    result['comment'] = COMMENTS[model_type][1][0]
             elif max_score < 2/3:
                 if pred_class == 'no_' + model_type:
-                    result['comment'] = '几乎没有'
+                    result['comment'] = COMMENTS[model_type][0][1]
                 else:
-                    result['comment'] = '可以看到'
+                    result['comment'] = COMMENTS[model_type][1][1]
             else:
                 if pred_class == 'no_' + model_type:
-                    result['comment'] = '没有迹象'
+                    result['comment'] = COMMENTS[model_type][0][2]
                 else:
-                    result['comment'] = '可见显著'
+                    result['comment'] = COMMENTS[model_type][1][2]
             
             # cv2.imwrite(f"inference_outputs/images/{image_name}.jpg", orig_image)
     result['infer_time'] = infer_time
